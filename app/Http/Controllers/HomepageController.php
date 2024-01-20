e33d65e565972a885a64256608cd2eae0b1c7763
Nghia <nghiait06>
2024-01-20 08:17:43 +0700

    update code

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -917,14 +917,14 @@ public function getDataInfo (Request $request)
         
         if($slug =="demo" || $slug =="soida")
         {
-            return view("resultNormal", compact("slug", 
+            return view("resultZalo", compact("slug", 
              "ageGame","ageGameReal","gameType","gameJoinType1",
              "contetnFail", "contentSuccess",  "agent","companyId", "displayGame", "rewardCheck", "turnOffGame","successGame","dataGame")); 
         }
         else 
         {
             
-            return view("resultZalo", compact("slug", 
+            return view("resultNormal", compact("slug", 
               
             "ageGame","ageGameReal","gameType","gameJoinType1",
              "contetnFail", "contentSuccess",  "agent","companyId", "displayGame", "rewardCheck", "turnOffGame","successGame","dataGame")); 
